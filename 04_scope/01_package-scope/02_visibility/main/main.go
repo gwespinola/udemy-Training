57d71cac138aa55a4382d34e0f40646e9a84930d
Gilberto Espinola <gwespinola@gmail.com>
2017-04-03 21:05:07 -0300

    Adding blank identifiers and constants

--- 04_scope/01_package-scope/02_visibility/main/main.go
+++ 04_scope/01_package-scope/02_visibility/main/main.go
@@ -1,7 +1,9 @@
 package main
 
-import 	("fmt"
-	"github.com/gwespinola/udemy-Training/04_scope/01_package-scope/02_visibility/vis")
+import (
+	"fmt"
+	"github.com/gwespinola/udemy-Training/04_scope/01_package-scope/02_visibility/vis"
+)
 
 func main() {
 	fmt.Println(vis.MyName)
