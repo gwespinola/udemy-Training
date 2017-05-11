1e537122a023264bd5f5e28c16a07fa729944ab6
Gilberto Espinola <gwespinola@gmail.com>
2017-05-11 12:05:21 -0300

    Project Euler completion

--- 16_exercises/06-euler/01_/main.go
+++ 16_exercises/06-euler/01_/main.go
@@ -10,3 +10,8 @@ func main() {
 	}
 	return
 }
+
+/*2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
