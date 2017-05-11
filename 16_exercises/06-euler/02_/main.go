f27953dee54c7095ed6d730790ee8d83be45ceec
Gilberto Espinola <gwespinola@gmail.com>
2017-05-11 11:44:27 -0300

    Completion of Project Euler

---
+++ 16_exercises/06-euler/02_/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func fibonacci(n uint) uint {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	} else {
+		return fibonacci(n-1) + fibonacci(n-2)
+	}
+}
+
+func main()  {
+	n := uint(3999999)
+	fmt.Println(fibonacci(n))
+
+}
