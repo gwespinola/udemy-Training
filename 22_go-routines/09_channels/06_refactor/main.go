daafaad87876213c0e072fa01ddaa33d4b28cd40
Gilberto Espinola <gwespinola@gmail.com>
2017-06-03 23:28:39 -0300

    Added comment

--- 22_go-routines/09_channels/06_refactor/main.go
+++ 22_go-routines/09_channels/06_refactor/main.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 	c := incrementor()
-	for n := range puller(c) {
-		fmt.Println(n)
-	}
+	for n := range puller(c) { // This code is differentiated from the one in the previous exercise, because here we used regular code substitution
+		fmt.Println(n) // and instead of having a variable "cSum" assigning the channel result to the expression range we can use the principle
+	} // of equality and substitute the variable "cSum" by the result of channel "puller(c)".
 }
 
 func incrementor() <-chan int {
