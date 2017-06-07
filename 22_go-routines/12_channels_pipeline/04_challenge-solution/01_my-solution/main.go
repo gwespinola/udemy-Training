4e8357bfe7bab25eb63ec8c43500b2813cd41788
Gilberto Espinola <gwespinola@gmail.com>
2017-06-06 21:50:44 -0300

    Pipeline Challenge

--- 22_go-routines/12_channels_pipeline/04_challenge-solution/01_my-solution/main.go
+++ 22_go-routines/12_channels_pipeline/04_challenge-solution/01_my-solution/main.go
@@ -3,29 +3,45 @@ package main
 import "fmt"
 
 func main() {
-	for n := range fac(gen(100)) {
+
+	in := gen()
+
+	f := factorial(in)
+
+	for n := range f{
 		fmt.Println(n)
 	}
 }
 
-func gen(nums ...int) chan int {
+func gen() <- chan int{
 	out := make(chan int)
 	go func() {
-		for nums := 0; nums > 0; nums-- {
-			out <- nums
+		for i := 0; i < 10; i++{
+			for j := 3; j < 13; j++{
+				out <- j
+			}
 		}
 		close(out)
 	}()
 	return out
 }
 
-func fac(in chan int) chan int {
+func factorial(in <- chan int) <-chan int {
 	out := make(chan int)
 	go func() {
 		for n := range in {
-			out <- n * n
+			out <- fact(n)
 		}
 		close(out)
 	}()
 	return out
+}
+
+func fact(n int) int {
+	total := 1
+	for i := n; i >0; i -- {
+		total *= i
+	}
+	return total
+
 }
