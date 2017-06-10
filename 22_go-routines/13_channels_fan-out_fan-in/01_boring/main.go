274730e2a8cc6cdbfa13eb89b369f16867e137e6
Gilberto Espinola <gwespinola@gmail.com>
2017-06-10 10:58:45 -0300

    First part of channels class is done

--- 22_go-routines/13_channels_fan-out_fan-in/01_boring/main.go
+++ 22_go-routines/13_channels_fan-out_fan-in/01_boring/main.go
@@ -7,39 +7,40 @@ import (
 )
 
 func main() {
-	c := fanIn(boring("Joe"), boring("Ann"))	// boring joe and boring ann both return a channel and fanIn takes two inputs
-	for i:= 0; i < 10; i++ {			// og the receive only channel and the channel received has strings on it.
-		fmt.Println(<-c)
+	c := fanIn(boring("Joe"), boring("Ann")) // boring joe and boring ann both return a channel and fanIn takes two inputs
+	for i := 0; i < 10; i++ {                // of the receive only channel and the channel received has strings on it.
+		fmt.Println(<-c) // This channel prevents the program from exiting.
 	}
 	fmt.Println("You are both boring; I'm leaving.")
 }
 
-func boring(msg string) <- chan string {	// This function takes a string and then it makes a channel.
+func boring(msg string) <-chan string { // This function takes a string and then it makes a channel.
 	c := make(chan string)
-	go func() {				// launches a go function and then returns the channel
-		for i := 0; ; i++ {		// then it writes a string to the channel
+	go func() { // launches a go function and then returns the channel
+		for i := 0; ; i++ { // then it writes a string to the channel
 			c <- fmt.Sprintf("%s %d", msg, i)
 			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 		}
 	}()
-	return c				// and that gets returned so we return a channel
+	return c // and that gets returned so we return a channel
 }
 
 // FAN IN
-func fanIn(input1, input2 <-chan string) <- chan string {	// This function takes two channels: input 1 and input2.
+func fanIn(input1, input2 <-chan string) <-chan string { // This function takes two channels: input 1 and input2.
 	c := make(chan string)
 	go func() {
 		for {
-			c <- <-input1				// We take the value of input1 and then puts it on c which
-		}						// is a brand new channel.
+			c <- <-input1 // We take the value of input1 and then puts it on c which
+		} // is a brand new channel.
 	}()
 	go func() {
 		for {
-			c <- <- input2
+			c <- <-input2
 		}
 	}()
 	return c
 }
+
 /*
 code source:
 Rob Pike
