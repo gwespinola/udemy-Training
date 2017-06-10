efdccd0f5c7778981c38cc614a06f73da2392ee6
Gilberto Espinola <gwespinola@gmail.com>
2017-06-10 11:01:58 -0300

    Corrected exercise 2 code

--- 22_go-routines/13_channels_fan-out_fan-in/02_sq-output/main.go
+++ 22_go-routines/13_channels_fan-out_fan-in/02_sq-output/main.go
@@ -16,12 +16,13 @@ func main() {
 	// FAN IN
 	// Consume the merged output from multiple channels.
 	for n := range merge(c1, c2) {
-		fmt.Println(n) //4 then 9, or 9 then 4
+		fmt.Println(n) // 4 then 9, or 9 then 4
 	}
 }
 
 func gen(nums ...int) chan int {
-	fmt.Printf("TYPE OF NUMS %T\n", nums) // Just FYI
+	fmt.Printf("TYPE OF NUMS %T\n", nums) // just FYI
+
 	out := make(chan int)
 	go func() {
 		for _, n := range nums {
@@ -44,9 +45,10 @@ func sq(in chan int) chan int {
 }
 
 func merge(cs ...chan int) chan int {
+	fmt.Printf("TYPE OF CS: %T\n", cs) // just FYI
+
 	out := make(chan int)
 	var wg sync.WaitGroup
-	fmt.Printf("TYPE OF CS: %T\n", cs) // Just FYI
 	wg.Add(len(cs))
 
 	for _, c := range cs {
@@ -62,5 +64,30 @@ func merge(cs ...chan int) chan int {
 		wg.Wait()
 		close(out)
 	}()
+
 	return out
 }
+
+/*
+FAN OUT
+Multiple functions reading from the same channel until that channel is closed
+
+FAN IN
+A function can read from multiple inputs and proceed until all are closed by
+multiplexing the input channels onto a single channel that's closed when
+all the inputs are closed.
+
+PATTERN
+there's a pattern to our pipeline functions:
+-- stages close their outbound channels when all the send operations are done.
+-- stages keep receiving values from inbound channels until those channels are closed.
+
+source:
+https://blog.golang.org/pipelines
+*/
+
+/*
+CHALLENGE #1
+When know HOW to do fan out / fan in, but do we know WHY?
+Why would we want to do fan out / fan in?
+*/
